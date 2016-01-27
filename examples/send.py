a22afde583df64ddd37d10287534557aa6408cf1
Mahmood S. Zargar <mahmood@gmail.com>
2016-01-27 01:02:51 -0500

    Update send.py

--- examples/send.py
+++ examples/send.py
@@ -15,7 +15,7 @@ radio = NRF24()
 radio.begin(1, 0, "P9_23", "P9_24") #Set CE and IRQ pins
 radio.setRetries(15,15)
 radio.setPayloadSize(8)
-radio.setChannel(0x60)
+radio.setChannel(76)
 
 radio.setDataRate(NRF24.BR_250KBPS)
 radio.setPALevel(NRF24.PA_MAX)
